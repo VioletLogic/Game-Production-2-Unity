70d0ba106456c23377c5f97bd503c511c8c29aa8
VioletLogic <VioletLapena@gmail.com>
2023-02-09 11:47:17 -0500

    initial commit

---
+++ Assets/Abilities/Dummy Base/DummyAbility.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+
+public class DummyAbility : MonoBehaviour
+{
+    public string SpriteName;
+    public int Damage = 1;
+    public int MaxHits = 1;
+    public float Duration = 1.0f;
+    public Vector2 Direction = Vector2.right;
+    public float Gravaty = 0;
+
+    public bool CollidesWithPlayer;
+    public bool CollidesWithPlayerProjectiles;
+    public bool CollidesWithEnemies;
+    public bool CollidesWithEnemyProjectiles;
+    public bool CollidesWithTerrain;
+    
+    public string RequitedEffect;
+    public string BlockedByEffect;
+    public string AppliesEffect;
+
+    [TextArea(5, 10)]
+    public string Notes;
+
+    //public Collider2D Collider;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Duration -= Time.fixedDeltaTime;
+        if (Duration <= 0)
+        {
+            Destroy(gameObject);
+        }
+
+        transform.position += Time.fixedDeltaTime * new Vector3(Direction.x, Direction.y, 0);
+        Direction.y += Gravaty * Time.fixedDeltaTime;
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log("Ability Hit");
+        Destroy(other.gameObject);
+        MaxHits--;
+        if (MaxHits <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
